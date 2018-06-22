01a380bc1d264574f2e794b937c1d7eddd45991a
Mark Heath <mark.heath@gmail.com>
2018-06-22 08:11:01 +0100

    using v2 style http functions with the new CreateHttpManagementPayload

--- VideoProcessorV2/ProcessVideoStarter.cs
+++ VideoProcessorV2/ProcessVideoStarter.cs
@@ -1,7 +1,6 @@
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -11,58 +10,59 @@ namespace VideoProcessor
     public static class ProcessVideoStarter
     {
         [FunctionName("ProcessVideoStarter")]
-        public static async Task<HttpResponseMessage> Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
-            HttpRequestMessage req,
+            HttpRequest req,
             [OrchestrationClient] DurableOrchestrationClient starter,
             TraceWriter log)
         {
             // parse query parameter
-            string video = req.RequestUri.ParseQueryString()["video"];
+            string video = req.GetQueryParameterDictionary()["video"];
 
             if (video == null)
             {
-                return req.CreateResponse(HttpStatusCode.BadRequest,
+                return new BadRequestObjectResult(
                    "Please pass the video location the query string");
             }
 
             log.Info($"About to start orchestration for {video}");
 
             var orchestrationId = await starter.StartNewAsync("O_ProcessVideo", video);
-
-            return starter.CreateCheckStatusResponse(req, orchestrationId);
+            var payload = starter.CreateHttpManagementPayload(orchestrationId);
+            return new OkObjectResult(payload);
         }
 
         [FunctionName("SubmitVideoApproval")]
-        public static async Task<HttpResponseMessage> SubmitVideoApproval(
+        public static async Task<IActionResult> SubmitVideoApproval(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "SubmitVideoApproval/{id}")]
-            HttpRequestMessage req,
+            HttpRequest req,
             [OrchestrationClient] DurableOrchestrationClient client,
             [Table("Approvals", "Approval", "{id}", Connection = "AzureWebJobsStorage")] Approval approval,
             TraceWriter log)
         {
             // nb if the approval code doesn't exist, framework just returns a 404 before we get here
-            string result = req.RequestUri.ParseQueryString()["result"];
+            string result = req.GetQueryParameterDictionary()["result"];
             
             if (result == null)
-                return req.CreateResponse(HttpStatusCode.BadRequest, "Need an approval result");
+                return new BadRequestObjectResult("Need an approval result");
 
             log.Warning($"Sending approval result to {approval.OrchestrationId} of {result}");
             // send the ApprovalResult external event to this orchestration
             await client.RaiseEventAsync(approval.OrchestrationId, "ApprovalResult", result);
 
-            return req.CreateResponse(HttpStatusCode.OK);
+            return new OkResult();
         }
 
         [FunctionName("StartPeriodicTask")]
-        public static async Task<HttpResponseMessage> StartPeriodicTask(
+        public static async Task<IActionResult> StartPeriodicTask(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
-            HttpRequestMessage req,
+            HttpRequest req,
             [OrchestrationClient] DurableOrchestrationClient client,
             TraceWriter log)
         {
             var instanceId = await client.StartNewAsync("O_PeriodicTask", 0);
-            return client.CreateCheckStatusResponse(req, instanceId);
+            var payload = client.CreateHttpManagementPayload(instanceId);
+            return new OkObjectResult(payload);
         }
     }
 }
